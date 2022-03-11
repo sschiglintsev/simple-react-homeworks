de5bb5d5d2aae5960d551b8c1bdaecccca6e48ef
sockromant <s.s.chiglintsev@gmail.com>
2022-03-11 14:39:10 +0500

    finish

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+export type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: MessageType) {
     return (
-        <div>
-
+        <div className={s.messages}>
+            <div  >
+            <img className={s.circle} src={props.avatar} />
+            </div>
+            <span className={s.ugol}></span>
+            < div className={s.message}>
+                <div className={s.name}>{props.name}</div>
+                <p className={s.p}>{props.message} <span className={s.span}>{props.time}</span></p>
+            </div>
         </div>
     )
 }
