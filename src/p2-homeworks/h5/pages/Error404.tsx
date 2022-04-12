34c576ec3e206d7d5d017c4fc9583f9e808c6f8b
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-12 17:47:04 +0500

    h5

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <h1 className="text-jumbo text-ginormous">Oops!</h1>
+            <div>Error code: 404</div>
+            <h2>We can't seem to find the page you're looking for</h2>
+            <ul className="list-unstyled">
+                <li>Here are some helpful links instead:</li>
+                <li><NavLink to={'/'} >pre_junior</NavLink></li>
+                <li><NavLink to={'/junior'}>junior</NavLink>
+                </li>
+                <li><NavLink to={'/junior-plus'}>junior+</NavLink></li>
+
+            </ul>
+
         </div>
     )
 }
