12da3dffccf5680125702ce056f4b6d14d7b00ca
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-13 00:14:46 +0500

    h3

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import {AffairType} from "./HW2";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 type AffairPropsType = {
-    key:number
+    key: number
     // key не нужно типизировать
     affair: AffairType // need to fix any
-    deleteAffairCallback: (id:number)=>void // need to fix any
+    deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = (t:number) => {
+    const deleteCallback = (t: number) => {
         props.deleteAffairCallback(t)
     }// need to fix
 
     return (
         <div key={props.key}>
             {props.affair.name} {props.affair.priority}
-
-            <button onClick={()=>deleteCallback(props.affair._id)}>X</button>
+            <SuperButton onClick={() => deleteCallback(props.affair._id)}>x</SuperButton>
         </div>
     )
 }
