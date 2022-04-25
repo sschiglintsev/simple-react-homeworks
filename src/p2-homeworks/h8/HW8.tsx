35fca269d32ccb1e1fb93f716dc767f87e321efc
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-25 16:37:03 +0500

    h8

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,12 +1,21 @@
 import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import {checkAC, homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 // export type UserType =
 
+export type man = {
+    _id:number,
+    name: string,
+    age:number
+}
+
+
+
+
 const initialPeople = [
     {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
+    {_id: 1, name: 'Александр', age: 17},
     {_id: 2, name: 'Коля', age: 16},
     {_id: 3, name: 'Виктор', age: 44},
     {_id: 4, name: 'Дмитрий', age: 40},
@@ -14,16 +23,19 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<Array<man>>(initialPeople) // need to fix any
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
+    const finalPeople = people.map((p: man) => (
         <div key={p._id}>
-            some name, age
+            <div style={{float:'left',width: '20%'}} >{p.name}</div>
+            <div>{p.age}</div>
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const check = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: ''}))
 
     return (
         <div>
@@ -33,9 +45,9 @@ function HW8() {
             {/*should work (должно работать)*/}
             {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div style={{float:'left'}}><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+            <div style={{float:'left'}}><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+            <div style={{float:'left'}}><SuperButton onClick={check}>check 18</SuperButton></div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
