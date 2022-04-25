1be616570e388b3fab9cd0d79c73c6a89c46065b
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-25 16:37:58 +0500

    h8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,50 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {man} from "../HW8";
+
+export const homeWorkReducer = (state: Array<man>, action: homeWorkerReducerType) => { // need to fix any
+
     switch (action.type) {
+
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'up') {
+                const stateCopy = state.map(el => el).sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1)
+                return stateCopy
+            } else {
+                const stateCopy = state.map(el => el).sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1)
+                return stateCopy
+            }
         }
         case 'check': {
-            // need to fix
-            return state
+
+            const stateCopy = state.filter(el => el.age > 18)
+            return stateCopy
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+type homeWorkerReducerType = sortUpACType | sortDownACType | checkACType
+
+type sortUpACType = ReturnType<typeof sortUpAC>
+export const sortUpAC = () => {
+    return {
+        type: 'sort',
+        payload: {
+            sort: 'up'
+        }
+    }
+}
+type sortDownACType = ReturnType<typeof sortDownAC>
+const sortDownAC = () => {
+    return {
+        type: 'sort',
+        payload: 'down'
+    }
+}
+type checkACType = ReturnType<typeof checkAC>
+export const checkAC = () => {
+    return {
+        type: 'check',
+        payload: {}
     }
 }
