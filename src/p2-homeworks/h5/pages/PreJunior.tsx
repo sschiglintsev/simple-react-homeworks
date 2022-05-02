345f63e881bb92559035bae9f6b509ad522523f8
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-05-02 16:53:02 +0500

    h7

--- src/p2-homeworks/h5/pages/PreJunior.tsx
+++ src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -6,11 +6,11 @@ import HW4 from '../../h4/HW4'
 import HW6 from "../../h6/HW6";
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 function PreJunior() {
     return (
         <div>
-
             <HW1/>
             <HW2/>
             <HW3/>
@@ -18,7 +18,7 @@ function PreJunior() {
             <HW6/>
             <HW7/>
             <HW8/>
-
+            <HW9/>
         </div>
     )
 }
